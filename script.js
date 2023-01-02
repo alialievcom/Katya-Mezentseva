18ca43ba87774756913f4d9629bae49e3e59c698
Ali Aliev <ali.aliev.msk>
2023-01-02 16:59:15 +0300

    initial

---
+++ script.js
@@ -0,0 +1,29 @@
+const clickx= document.getElementById('pencet');
+const clickx2= document.getElementById('links-hide-menu');
+
+clickx.addEventListener('click', function(){
+    clickx.classList.toggle('Diam');
+    clickx2.classList.toggle('links-menu');
+});
+
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+});
+
+// const box = document.querySelector('.link');
+//
+// box.addEventListener('mouseover', function handleMouseOver() {
+//     box.style.color = 'red';
+// });
