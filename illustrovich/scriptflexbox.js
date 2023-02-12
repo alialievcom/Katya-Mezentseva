00967733dbdb95aea050b45e2d52eb54f2a5c3a8
Ali Aliev <ali.aliev.msk>
2023-02-12 16:28:44 +0300

    final changes

--- illustrovich/scriptflexbox.js
+++ illustrovich/scriptflexbox.js
@@ -11,13 +11,16 @@ function MenuElements(value, key) {
         if(document.getElementById(value).classList.contains('grid_show') === false){
             value1.classList.toggle('grid_show');
             value1.classList.remove('grid_hide');
+            key1.classList.remove("arrow-line")
             key1.querySelector("img").classList.remove("down")
             key1.querySelector("img").classList.toggle("up")
             key1.getElementsByClassName("line_upper")[0].classList.toggle("hide")
 
+
         } else {
             value1.classList.toggle('grid_hide');
             value1.classList.remove('grid_show');
+            key1.classList.toggle("arrow-line")
             key1.querySelector("img").classList.remove("up")
             key1.querySelector("img").classList.toggle("down")
             key1.getElementsByClassName("line_upper")[0].classList.remove("hide")
